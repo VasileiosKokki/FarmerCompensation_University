a1ea8bf5b406fb0d3ec29c5c311e9117583747ae
Inlavid <it2021042@hua.gr>
2024-04-30 21:34:17 +0300

    revert

--- src/main/java/dit/hua/distributedSystems/project/demo/repository/ApplicationRepository.java
+++ src/main/java/dit/hua/distributedSystems/project/demo/repository/ApplicationRepository.java
@@ -3,10 +3,9 @@ package dit.hua.distributedSystems.project.demo.repository;
 import dit.hua.distributedSystems.project.demo.entity.Application;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path= "application")
+@Repository
 @Hidden
 public interface ApplicationRepository  extends JpaRepository<Application, Integer> {
 }
--- src/main/java/dit/hua/distributedSystems/project/demo/repository/UserRepository.java
+++ src/main/java/dit/hua/distributedSystems/project/demo/repository/UserRepository.java
@@ -3,12 +3,11 @@ package dit.hua.distributedSystems.project.demo.repository;
 import dit.hua.distributedSystems.project.demo.entity.MUser;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@RepositoryRestResource(path= "user")
+@Repository
 @Hidden
 public interface UserRepository  extends JpaRepository<MUser, Integer> {
 
