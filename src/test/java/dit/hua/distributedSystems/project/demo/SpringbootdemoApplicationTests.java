c480447721e3afaffdab2b423122c0fd85863415
Inlavid <it2021042@hua.gr>
2024-05-18 18:24:43 +0300

    added mvnw test

--- src/test/java/dit/hua/distributedSystems/project/demo/SpringbootdemoApplicationTests.java
+++ src/test/java/dit/hua/distributedSystems/project/demo/SpringbootdemoApplicationTests.java
@@ -2,12 +2,42 @@ package dit.hua.distributedSystems.project.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringbootdemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Test
+	public void testSignUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"admin\",\"password\":\"pavlosnikolopoulos44\"}";
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signin")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk())
+				.andExpect(jsonPath("$.username").value("admin"));
+
 	}
 
 }
